d343bce02a05703923cffeda3435415dca6be61e
lex <lexinhell@gmail.com>
2016-03-17 18:07:52 +0300

    first

---
+++ js/work.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $("#title-time-100").click(function() {
+        $("#data-time-100").slideToggle('slow');
+    });
+    $("#title-mem-100").click(function() {
+        $("#data-mem-100").slideToggle('slow');
+    });
+});
